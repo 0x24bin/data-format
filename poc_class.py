c354fd7aa466b15fee08eb7aed2d445189fc7ce6
fiht <fiht@qq.com>
2016-06-18 15:54:01 +0800

    更改了识别sql注入的规则

--- poc_class.py
+++ poc_class.py
@@ -39,9 +39,29 @@ class poc(object):
                 return_list.append(i)
                 url_set.add(netloc)
         return return_list
-sql_injection = poc(name='sql_injection',key_word='=',expect_word='http') # 这里加expect_word 有一个想法是 只检测本域的url
+########################################################################
+class Sql_injection(poc):
+    """主要重写了url去重的规则"""
+    #----------------------------------------------------------------------
+    def list_format(self,url_list):
+        """规则应该是->每个路径下都取一个,比如www.baidu.com/aaa/text?a=1"""
+        from urllib.parse import urlparse
+        url_set = set()
+        return_list = []
+        for i in url_list:
+            parseResult = urlparse(i)
+            netloc = parseResult[1]
+            path = parseResult[2]
+            if netloc+path not in url_set:
+                return_list.append(i)
+                url_set.add(netloc+path)
+        return return_list
+sql_injection = Sql_injection(name='sql_injection',key_word='=',expect_word='http') # 这里加expect_word 有一个想法是 只检测本域的url
 st2_ = poc(name='struts2',key_word='.do .action',expect_word='.doc www.do')
 file_get = poc(name='任意文件下载',key_word='filename= file=')
 login_url = poc(name='敏感链接',key_word='login admin',)
 # 现在只添加了三个特征库,需要的话可以添加更多
 # 每个类负责检测去重属于自己的类型
+test_list = ['/portal/?user_type=zxxs', '/portal/?user_type=jzyg', '/portal/?user_type=sdxy', '/portal/?user_type=hzhb', '/portal/?user_type=zsdr', '/portal/?user_type=shfk']
+if __name__=='__main__':
+    print(test_list)
